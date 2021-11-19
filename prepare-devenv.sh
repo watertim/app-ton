a1eb6fbcb1ac622e63ade368f6679c244072f8de
WaterTim <a70546153@gmail.com>
2021-11-20 02:02:28 +0800

    update makefile & add file exist detection in .sh

--- prepare-devenv.sh
+++ prepare-devenv.sh
@@ -20,24 +20,34 @@ mkdir dev-env/CC/nanox
 
 cd tmp
 
-wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2 -O gcc-arm-none-eabi.tar.bz2 &&
+if [ ! -e "gcc-arm-none-eabi.tar.bz2" ]; then
+    wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2 -O gcc-arm-none-eabi.tar.bz2
+fi
 tar xf gcc-arm-none-eabi.tar.bz2 &&
 cp -r gcc-arm-none-eabi-10-2020-q4-major ../dev-env/CC/nanox/gcc-arm-none-eabi-10 &&
 mv gcc-arm-none-eabi-10-2020-q4-major ../dev-env/CC/others/gcc-arm-none-eabi-10
 
-wget http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.10.tar.xz -O clang+llvm.tar.xz &&
-tar xf clang+llvm.tar.xz &&
+if [ ! -e "clang+llvm_4.tar.xz" ]; then
+    wget http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.10.tar.xz -O clang+llvm_4.tar.xz 
+fi
+tar xf clang+llvm_4.tar.xz &&
 mv clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.10 ../dev-env/CC/others/clang-arm-fropi
 
-wget http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -O clang+llvm.tar.xz &&
-tar xf clang+llvm.tar.xz &&
+if [ ! -e "clang+llvm_9.tar.xz" ]; then
+    wget http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -O clang+llvm_9.tar.xz 
+fi
+tar xf clang+llvm_9.tar.xz &&
 mv clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04 ../dev-env/CC/nanox/clang-arm-fropi
 
-wget https://github.com/LedgerHQ/nanos-secure-sdk/archive/refs/tags/2.0.0-1.tar.gz -O nanos-secure-sdk.tar.gz &&
+if [ ! -e "nanos-secure-sdk.tar.gz" ]; then
+    wget https://github.com/LedgerHQ/nanos-secure-sdk/archive/refs/tags/2.0.0-1.tar.gz -O nanos-secure-sdk.tar.gz 
+fi
 tar xf nanos-secure-sdk.tar.gz &&
 mv nanos-secure-sdk-2.0.0-1 ../dev-env/SDK/nanos-secure-sdk
 
-wget https://github.com/LedgerHQ/nanox-secure-sdk/archive/refs/tags/2.0.0.tar.gz -O nanox-secure-sdk.tar.gz &&
+if [ ! -e "nanox-secure-sdk.tar.gz" ]; then
+    wget https://github.com/LedgerHQ/nanox-secure-sdk/archive/refs/tags/2.0.0.tar.gz -O nanox-secure-sdk.tar.gz 
+fi
 tar xf nanox-secure-sdk.tar.gz &&
 mv nanox-secure-sdk-2.0.0 ../dev-env/SDK/nanox-secure-sdk
 
